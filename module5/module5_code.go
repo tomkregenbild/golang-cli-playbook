a9de8c7af10ff96ce241cacd0e0a13255a4399ec
Tom Kregenbild <tom@MININT-KEQFTI2.middleeast.corp.microsoft.com>
2021-10-01 00:44:33 +0300

    Completed module

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,16 +1,12 @@
 package module5
 
-import (
-	"fmt"
-	"net/http"
-)
-
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
+/*
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
-		fmt.Println("something went wrong")
-	}
+	@@ -10,4 +14,3 @@ func GetExampleDotCom() {
 
 	defer resp.Body.Close()
 }
+*/
